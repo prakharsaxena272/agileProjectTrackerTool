a1625dd3d5bbeb712c8fdc39c7518a190240e485
prakhar <prakharsaxena303@gmail.com>
2022-05-31 00:34:46 +0530

    first commit

---
+++ projectTrackerTool/src/main/java/com/readyMadeProjects/projectTrackerTool/ProjectTrackerToolApplication.java
@@ -0,0 +1,13 @@
+package com.readyMadeProjects.projectTrackerTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackerToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackerToolApplication.class, args);
+	}
+
+}
---
+++ projectTrackerTool/src/test/java/com/readyMadeProjects/projectTrackerTool/ProjectTrackerToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readyMadeProjects.projectTrackerTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
